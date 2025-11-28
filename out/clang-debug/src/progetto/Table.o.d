00dd19ce9ff6e088f2f36d3e2ee70c1012e0bf8b
lrotelli01 <l.rotelli@studenti.unipi.it>
2025-11-28 15:06:41 +0100

    Add User class implementation and DatabaseNetwork NED file
    - Created User class with associated header file.
    - Implemented DatabaseNetwork NED file to define the network structure.
    - Configured parameters for users and tables, and established connections between them.

---
+++ out/clang-debug/src/progetto/Table.o.d
@@ -0,0 +1,4 @@
+../out/clang-debug/src/progetto/Table.o: progetto/Table.cc \
+  progetto/Table.h /home/luca/Scaricati/omnetpp-6.2.0/include/omnetpp.h
+progetto/Table.h:
+/home/luca/Scaricati/omnetpp-6.2.0/include/omnetpp.h:
